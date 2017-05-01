c83546f789979b8d0c3a1088c652b17866acca0d
Protesilaos Stavrou <info@protesilaos.com>
2017-05-01 10:37:43 +0300

    Add helper script for pacman backups

---
+++ my-utils/backup-pacman-packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo pacman -Qq > $HOME/backups/pacman/all.txt &
+sudo pacman -Qnq > $HOME/backups/pacman/native.txt &
+sudo pacman -Qmq > $HOME/backups/pacman/foreign.txt &
+sudo pacman -Qqe > $HOME/backups/pacman/explicit.txt &
+sudo pacman -Qqd > $HOME/backups/pacman/deps.txt &
+sudo pacman -Qqg > $HOME/backups/pacman/groups.txt &
+cp -r $HOME/backups/pacman/ $HOME/dots/
