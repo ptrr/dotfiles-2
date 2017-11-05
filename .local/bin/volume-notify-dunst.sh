1211925856ae9c4dbd39d276c908b449f68fa33f
Protesilaos Stavrou <info@protesilaos.com>
2017-11-05 10:34:33 +0200

    Minor refinements

--- .local/bin/volume-notify-dunst.sh
+++ .local/bin/volume-notify-dunst.sh
@@ -18,9 +18,9 @@ function send_notification {
     volume=`get_volume`
     # Make the bar with the special character ─ (it's not dash -)
     # https://en.wikipedia.org/wiki/Box-drawing_character
-    bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g')
+    bar=$(seq -s "━" $(($volume / 5)) | sed 's/[0-9]//g')
     # Send the notification
-    dunstify -I /usr/share/icons/Papirus-Adapta-Nokto/64x64/devices/audio-speakers.svg -t 8 -r 2593 -u normal "$bar"
+    dunstify -I /usr/share/icons/Papirus-Adapta-Nokto/64x64/devices/audio-speakers.svg -t 3 -r 2593 -u normal "$bar"
 }
 
 case $1 in
