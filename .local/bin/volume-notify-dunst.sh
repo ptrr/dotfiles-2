3adda6b8fbabab3306d423e2a7028f02aeed9286
Protesilaos Stavrou <info@protesilaos.com>
2017-11-07 18:50:43 +0200

    Refinements

--- .local/bin/volume-notify-dunst.sh
+++ .local/bin/volume-notify-dunst.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-# Source https://gist.github.com/sebastiencs/5d7227f388d93374cebdf72e783fbd6a
+# Display volume change notifications using dunst (dunstify)
+# Based on https://gist.github.com/sebastiencs/5d7227f388d93374cebdf72e783fbd6a
+
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # You can call this script like this:
-# $./volume.sh up
-# $./volume.sh down
-# $./volume.sh mute
+# $./path/to/script.sh up
+# $./path/to/script.sh down
+# $./path/to/script.sh mute
 
 function get_volume {
-    amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
+    amixer get Master | grep % -m 1 | awk '{print $5}' | sed 's/[^0-9\%]//g'
 }
 
 function is_mute {
@@ -16,28 +30,30 @@ function is_mute {
 
 function send_notification {
     volume=`get_volume`
-    # Make the bar with the special character ─ (it's not dash -)
-    # https://en.wikipedia.org/wiki/Box-drawing_character
-    bar=$(seq -s "━" $(($volume / 5)) | sed 's/[0-9]//g')
+
+    # Print volume status
+    status=$(amixer get Master | grep % -m 1 | awk '{print $5}' | sed 's/[^0-9\%]//g')
+
     # Send the notification
-    dunstify -I /usr/share/icons/Papirus-Adapta-Nokto/64x64/devices/audio-speakers.svg -t 3 -r 2593 -u normal "$bar"
+    dunstify -I /usr/share/icons/Papirus-Adapta-Nokto/64x64/devices/audio-speakers.svg -t 3 -r 2593 -u normal "Volume at $status"
 }
 
+# Either increase or decrease will turn the volume on if it was muted
 case $1 in
     up)
-	# Set the volume on (if it was muted)
+	# Increase the volume (+ 5%)
 	amixer -D pulse set Master on > /dev/null
-	# Up the volume (+ 5%)
 	amixer -D pulse sset Master 5%+ > /dev/null
 	send_notification
 	;;
     down)
+	# Decrease the volume (+ 5%)
 	amixer -D pulse set Master on > /dev/null
 	amixer -D pulse sset Master 5%- > /dev/null
 	send_notification
 	;;
     mute)
-    	# Toggle mute
+    # Toggle mute
 	amixer -D pulse set Master 1+ toggle > /dev/null
 	if is_mute ; then
 	    dunstify -i /usr/share/icons/Papirus-Adapta-Nokto/64x64@2x/apps/xfpm-suspend.svg -t 8 -r 2593 -u normal "Sound Muted"
